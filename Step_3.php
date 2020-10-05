703daf956a1c6d27bd12f0cf9d7aed6e429ce07c
Antoineghaddad <antoinehd95@gmail.com>
2020-10-05 14:33:33 +0300

    Step_3.php

---
+++ Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "tony";
+$email = "antoinehd95@gmail.com";
+echo "My name is   $name  and my email is  $email";
+$height = 200;
+$width = 100;
+$area = $height * $width;
+echo $area;
+?>

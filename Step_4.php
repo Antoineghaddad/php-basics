892b6a4bc812bde33a2ae20deeacaa3c18359ae7
Antoineghaddad <antoinehd95@gmail.com>
2020-10-05 15:05:33 +0300

    Step_4.php

---
+++ Step_4.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'tony';
+$position = 'consultant';
+$github_url = 'www.github.com';
+echo "Hello, I'm ". $name ." I'm a ". $position ." please check my github link ". $github_url;
+echo "<br>Hello, I'm  $name  I'm a  $position  please check my github link  $github_url";
+echo '<br>Hello, I\'m '. $name . ' I\'m a ' . $position  . '  please check my github link ' . $github_url;
+
+?>
